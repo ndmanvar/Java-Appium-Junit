2977f215ef1f664291e1c475569d4468c6625a8f
Neil <neil@Neils-MacBook-Pro.local>
2015-05-06 16:33:26 -0700

    Using env var

--- src/test/java/com/yourcompany/SampleSauceTest.java
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -34,7 +34,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
      * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
      * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
      */
-    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("YOUR_USERNAME", "YOUR_ACCESS_KEY");
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(System.getenv("SAUCE_USERNAME"), System.getenv("SAUCE_ACCESS_KEY"));
 
     /**
      * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
